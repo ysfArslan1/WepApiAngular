b21ac4d46385b6d12fb90ef753a5c2d66f6976b0
ysfArslan1 <ysf.arslan2017@gmail.com>
2024-02-07 00:36:39 +0300

    genreList added

---
+++ AngularFullStackUI/AngularFullStack.UI/src/app/features/genres/genre-list/genre-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { GenreResponse } from '../models/genre-responce.model';
+import { GenresServiceService } from '../services/genres.service.service';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-genre-list',
+  standalone: true,
+  imports: [RouterModule,CommonModule,HttpClientModule,RouterOutlet],
+  templateUrl: './genre-list.component.html',
+  styleUrl: './genre-list.component.css'
+})
+export class GenreListComponent {
+  genres: GenreResponse[]=[]
+
+  booksService = inject(GenresServiceService)
+  constructor(){}
+  ngOnInit(): void{
+    
+    this.booksService.getAllGenres()
+    .subscribe({
+      next: (data) => {
+        if (data && data.length > 0) {
+          this.genres = data;
+        }
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+}
