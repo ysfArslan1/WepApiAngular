738330b6cc502b01e89fd2557707f13cd82a9c0a
ysfArslan1 <ysf.arslan2017@gmail.com>
2024-02-08 23:17:51 +0300

    author-update componenet added

---
+++ AngularFullStackUI/AngularFullStack.UI/src/app/features/authors/author-update/author-update.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AuthorUpdateComponent } from './author-update.component';
+
+describe('AuthorUpdateComponent', () => {
+  let component: AuthorUpdateComponent;
+  let fixture: ComponentFixture<AuthorUpdateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AuthorUpdateComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AuthorUpdateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
